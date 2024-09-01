60bb773224eb71f680dcbdba733527a0b141d16a
Suraj <surajutt@iitbhilai.ac.in>
2024-09-01 17:25:14 +0530

    jobUpdate functionality added

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,6 +1,6 @@
 //importing all the necessary dependencies 
 import express from "express";
-import {jobCreate} from "../controllers/jobs.controllers.js"
+import {jobCreate,jobUpdate} from "../controllers/jobs.controllers.js"
 
 //using the router
 const router = express.Router();
@@ -8,6 +8,8 @@ const router = express.Router();
 //making the routes
 router.route("").get();
 router.post("/jobs",jobCreate)
+router.put("/jobs/:id",jobUpdate)
+
 
 //exporting the router
 export default router;
