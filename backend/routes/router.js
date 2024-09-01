12f69f422217d73c5809427aaf92a0f2cb60d0f8
Uday Bhardwaj <udaybh07@gmail.com>
2024-09-01 18:19:37 +0530

    Api for fetching student details

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,6 +1,7 @@
 //importing all the necessary dependencies 
 import express from "express";
 import {jobCreate,jobUpdate} from "../controllers/jobs.controllers.js"
+import { ViewRes } from "../controllers/studentview.js";
 
 //using the router
 const router = express.Router();
@@ -10,6 +11,8 @@ router.route("").get();
 router.post("/jobs",jobCreate)
 router.put("/jobs/:id",jobUpdate)
 
+// getting student view
+router.get('/student/:ID', ViewRes);
 
 //exporting the router
 export default router;
